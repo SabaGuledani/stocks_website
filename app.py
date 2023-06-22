f4080edee1eebea8c4da16097ebf9e9d1c3a2606
AniSuxita <ana.sukhitashvili.1@btu.edu.ge>
2023-06-22 21:15:55 +0400

    changes in stocks.py and app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask,redirect,url_for,render_template
+
+import stocks
 from models import db,Global_funds
+
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///GlobalFunds.sqlite'
@@ -9,7 +13,6 @@ with app.app_context():
     db.create_all()
 
 
-
 @app.route('/<ticker>')
 
 def home(ticker):
@@ -17,5 +20,21 @@ def home(ticker):
 
     return render_template('main.html')
 
+
+@app.route('/stockinfo')
+def stockinfo():
+    return render_template('stockinfo.html',
+                           previous_close = stocks.previous_close,
+                           industry = stocks.industry,
+                           stock_open = stocks.stock_open,
+                           d_range = stocks.d_range,
+                           volume = stocks.volume,
+                           after_hours = stocks.after_hours
+                           )
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
