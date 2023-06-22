e24531d01b69d348d441aa96e140a14a337ef6f4
saba_guledani <saba.guledani.1@btu.edu.ge>
2023-06-22 21:20:17 +0400

    jinja commit(stable)

--- app.py
+++ app.py
@@ -10,6 +10,34 @@ with app.app_context():
     db.create_all()
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @app.route('/')
 def home():
     us = Global_funds.query.filter_by(date=str(date.today())).filter_by(header='us').all()
@@ -17,7 +45,6 @@ def home():
     asia = Global_funds.query.filter_by(date=str(date.today())).filter_by(header='asia').all()
 
     return render_template('main.html', us=us, eu=eu, asia=asia)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
