8461a840dc719bcec0fe1e20ab4111e56ca362fd
AniSuxita <ana.sukhitashvili.1@btu.edu.ge>
2023-06-22 22:01:12 +0400

    Little hehee

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from flask import Flask,redirect,url_for,render_template
-
-import stocks
 from models import db,Global_funds
 
 
@@ -21,16 +19,16 @@ def home(ticker):
     return render_template('main.html')
 
 
-@app.route('/stockinfo')
-def stockinfo():
-    return render_template('stockinfo.html',
-                           previous_close = stocks.previous_close,
-                           industry = stocks.industry,
-                           stock_open = stocks.stock_open,
-                           d_range = stocks.d_range,
-                           volume = stocks.volume,
-                           after_hours = stocks.after_hours
-                           )
+# @app.route('/stockinfo')
+# def stockinfo():
+#     return render_template('stockinfo.html',
+#                            previous_close = stocks.previous_close,
+#                            industry = stocks.industry,
+#                            stock_open = stocks.stock_open,
+#                            d_range = stocks.d_range,
+#                            volume = stocks.volume,
+#                            after_hours = stocks.after_hours
+#                            )
 
 #ba
 if __name__ == '__main__':
